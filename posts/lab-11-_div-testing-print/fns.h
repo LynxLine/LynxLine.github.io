6246f5495f97871c7f46da380b4f048d22a9899c
yshurik <yshurik@gmail.com>
2024-05-21 11:43:05 +0200

    2024-05-21

---
+++ posts/lab-11-_div-testing-print/fns.h
@@ -0,0 +1,17 @@
+
+#define KADDR(p)    ((void *)p)
+#define PADDR(p)    ((ulong)p)
+#define waserror()  (up->nerrlab++, setlabel(&up->errlab[up->nerrlab-1]))
+#define	coherence()		/* nothing needed for uniprocessor */
+#define procsave(p)		/* Save the mach part of the current */
+						/* process state, no need for one cpu */
+
+void	(*serwrite)(char*, int);
+void    (*screenputs)(char*, int);
+
+#include "../port/portfns.h"
+
+void	pl011_putc(int);
+void	pl011_puts(char *);
+void	pl011_serputs(char *, int);
+void	pl011_addr(void *a, int nl);

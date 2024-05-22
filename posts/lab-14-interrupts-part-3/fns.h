0d8c8c75fea4c5d91e60ab783f00a35843a145c1
yshurik <yshurik@gmail.com>
2024-05-22 10:29:35 +0200

    2024-05-22

---
+++ posts/lab-14-interrupts-part-3/fns.h
@@ -0,0 +1,30 @@
+
+#define KADDR(p)    ((void *)p)
+#define PADDR(p)    ((ulong)p)
+#define waserror()  (up->nerrlab++, setlabel(&up->errlab[up->nerrlab-1]))
+#define	coherence()		/* nothing needed for uniprocessor */
+#define procsave(p)		/* Save the mach part of the current */
+						/* process state, no need for one cpu */
+
+void	(*serwrite)(char*, int);
+void    (*screenputs)(char*, int);
+
+#include "../port/portfns.h"
+
+void	pl011_putc(int);
+void	pl011_puts(char *);
+void	pl011_serputs(char *, int);
+void	pl011_addr(void *a, int nl);
+
+ulong	getsp(void);
+ulong	getcpuid(void);
+
+void	trapinit(void);
+char *	trapname(int psr);
+int		isvalid_va(void *v);
+int		isvalid_wa(void *v);
+void	setr13(int, void*);
+void	vectors(void);
+void	vtable(void);
+void	dumpregs(Ureg*);
+int		(*breakhandler)(Ureg*, Proc*);
---
+++ posts/lab-14-interrupts-part-3/main.c
@@ -0,0 +1,99 @@
+
+#include "u.h"
+#include "../port/lib.h"
+#include "dat.h"
+#include "mem.h"
+#include "fns.h"
+#include "version.h"
+
+#include "../port/uart.h"
+PhysUart* physuart[1];
+
+Conf conf;
+Mach *m = (Mach*)MACHADDR;
+Proc *up = 0;
+
+extern int main_pool_pcnt;
+extern int heap_pool_pcnt;
+extern int image_pool_pcnt;
+
+void
+confinit(void)
+{
+	ulong base;
+	conf.topofmem = 128*MB;
+
+	base = PGROUND((ulong)end);
+	conf.base0 = base;
+
+	conf.npage1 = 0;
+	conf.npage0 = (conf.topofmem - base)/BY2PG;
+	conf.npage = conf.npage0 + conf.npage1;
+	conf.ialloc = (((conf.npage*(main_pool_pcnt))/100)/2)*BY2PG;
+
+	conf.nproc = 100 + ((conf.npage*BY2PG)/MB)*5;
+	conf.nmach = 1;
+
+	print("Conf: top=%lud, npage0=%lud, ialloc=%lud, nproc=%lud\n",
+			conf.topofmem, conf.npage0,
+			conf.ialloc, conf.nproc);
+}
+
+static void
+poolsizeinit(void)
+{
+	ulong nb;
+	nb = conf.npage*BY2PG;
+	poolsize(mainmem, (nb*main_pool_pcnt)/100, 0);
+	poolsize(heapmem, (nb*heap_pool_pcnt)/100, 0);
+	poolsize(imagmem, (nb*image_pool_pcnt)/100, 1);
+}
+
+void
+main() {
+	pl011_puts("Entered main() at ");
+	pl011_addr(&main, 0);
+	pl011_puts(" with SP=");
+	pl011_addr((void *)getsp(), 1.);
+
+	pl011_puts("Clearing Mach:  ");
+	memset(m, 0, sizeof(Mach));
+	pl011_addr((char *)m,		0); pl011_puts("-");
+	pl011_addr((char *)(m+1),	1);
+
+	pl011_puts("Clearing edata: ");
+	memset(edata, 0, end-edata);
+	pl011_addr((char *)&edata,	0); pl011_puts("-");
+	pl011_addr((char *)&end,	1);
+
+	conf.nmach = 1;
+	serwrite = &pl011_serputs;
+
+	confinit();
+	xinit();
+	poolinit();
+	poolsizeinit();
+	trapinit();
+	printinit();
+
+	print("\nARM %ld MHz id %8.8lux\n", (m->cpuhz+500000)/1000000, getcpuid());
+	print("Inferno OS %s Vita Nuova\n", VERSION);
+
+	pl011_puts("to inifinite loop\n\n");
+	for (;;);
+}
+
+void	segflush(void*, ulong) { return; }
+void	idlehands(void) { return; }
+void 	kprocchild(Proc *p, void (*func)(void*), void *arg) { return; }
+
+void	exit(int) { return; }
+void	reboot(void) { return; }
+void	halt(void) { return; }
+
+Timer*	addclock0link(void (*)(void), int) { return 0; }
+void	clockcheck(void) { return; }
+
+void	fpinit(void) {}
+void	FPsave(void*) {}
+void	FPrestore(void*) {}
